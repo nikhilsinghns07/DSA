e563fb4223fdfa6abf0cc5608c085ac27ce72f85
Nikhil Kumar Singh <nikhilsinghns01@gmail.com>
2022-10-26 19:56:42 +0530

    pattern questions

--- DSA in C++ with CodeHelp/pattern2.cpp
+++ DSA in C++ with CodeHelp/pattern2.cpp
@@ -22,23 +22,131 @@ void pattern8() {
 
 void pattern9() {
     int n;
-    cin>>n;
+    cin>> n;
 
     int row = 1;
-    while(row <=n ){
-        int col = 1;
+    char ch = 'A';
 
-        while(col <=n) {
-            char ch = 'A' + row ;
-            cout<< ch;
+    while (row<=n)
+    {
+        int col = 1;
+         
+        while(col<= n) {
+            cout << ch << " ";
+            ch = ch + 1;
             col = col +1;
         }
+        cout << endl;
+        row = row + 1;
+    }
+    
+}
+
+
+void pattern10() {
+    int n;
+    cin>> n;
+
+    int row = 1;
+    
+    
+    while(row <= n) {
+        int col = 1;
+        
+        while(col <= n) {
+            char ch = 'A' + row +col -2;
+            cout << ch << " ";
+            col = col +1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+    
+}
+
+
+void pattern11() {
+    int n;
+    cin>> n;
+
+    int row = 1;
+    while(row<=n) {
+        int col = 1;
+        
+        while(col<=row){
+            char ch = 'A' + row - 1;
+            cout << ch << " ";
+            
+            col = col +1;
+        }
+
         cout << endl;
         row = row +1;
     }
 }
 
+void pattern12() {
+    int n;
+    cin>> n;
+
+    int row = 1;
+    char ch = 'A';
+    while(row<=n) {
+
+        int col = 1;
+        
+        
+
+        while(col<=row) {
+            cout << ch << " ";
+            ch = ch+1;    
+            col = col+1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+}
+
+void pattern13() {
+    int n;
+    cin>> n;
+
+    int row = 1;
+    
+    while(row<=n) {
+
+        int col = 1;
+        while(col<=row) {
+            char ch = 'A' + row + col -2;
+            cout << ch << " ";
+            col = col+1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+}
+
+void pattern14() {
+    int n;
+    cin >> n;
+    int row = 1;
+    
+    while(row<=n) {
+        int col = 1;
+        char ch = 'A' + n -row;
+        while(col<=row) {
+            cout <<  ch;
+            ch = ch +1;
+            col = col+1;
+        }
+
+        cout << endl;
+        row = row +1;
+
+    }
+}
+
 
 int main() {
-    pattern8();
+    pattern14();
 }
---
+++ DSA in C++ with CodeHelp/pattern3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+void pattern1 () {
+    int n;
+    cin >> n;
+    int row = 1;
+    while(row<=n) {
+        int space = n - row;
+        while(space) {
+            cout << " ";
+            space = space -1;
+
+        }
+        int col = 1;
+        while(col<= row) {
+            cout << "*";
+            col = col +1;
+        }
+        cout << endl;
+        row = row +1;
+
+    }
+}
+
+int main() {
+    pattern1();
+}
